19b73d14de2e0b0841bc84e8202a89dfe40a3285
김동우 <kimdongwoo0930@gmail.com>
2022-05-20 21:04:36 +0900

    10026: 적록색약 (Java)

---
+++ Java/10026/src/Main.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static String[][] photo;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        photo = new String[N][N];
+        for(int i=0;i<N;i++){
+            String[] D = br.readLine().split(" ");
+
+        }
+
+    }
+
+}
